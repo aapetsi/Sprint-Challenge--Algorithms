a739022233ceb4ddd9eaac1acef6d83ad05a1b88
aapetsi <apetsiampiah@gmail.com>
2020-02-21 09:10:53 +0000

    fix linting

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,6 +1,7 @@
 '''
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurences of ***"th"***
+occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
 
@@ -9,30 +10,20 @@ def count_th(word):
     result = []
     if len(word) < 2:
         return 0
-    
-    # result = 0
-    
-        
+
     def helper(val):
         if len(val) < 2:
             return
 
-        l = list(val)
-        th = l[0] + l[1]
+        word_list = list(val)
+        th = word_list[0] + word_list[1]
         if th == 'th':
-            # result += 1
-            print('yes')
             result.append(1)
-        l.pop(0)
-        new_word = ''.join(l)
+        word_list.pop(0)
+        new_word = ''.join(word_list)
 
-        helper(l)
- 
+        helper(word_list)
+
     helper(word)
 
-
-    
-    return  len(result)
-
-
-print(count_th('ththth'))
+    return len(result)
